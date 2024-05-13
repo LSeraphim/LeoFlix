bd375a23a6ea86a409bfbe4a58e2d0566cd276d3
LSeraphim <leostart133@gmail.com>
2024-05-13 20:31:06 -0300

    Adicionado classe de usuário e código para popular usuários

--- LeoFlix/Data/AppDbContext.cs
+++ LeoFlix/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
 using LeoFlix.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeoFlix.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
+    public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<Genre> Genres { get; set; }
     public DbSet<Movie> Movies { get; set; }
     public DbSet<MovieGenre> MovieGenres { get; set; }
@@ -31,6 +34,92 @@ public class AppDbContext : DbContext
             .HasOne(mg => mg.Genre)
             .WithMany(g => g.Movies)
             .HasForeignKey(mg => mg.GenreId);
+        #endregion
+
+        #region Popular usuário
+        List<IdentityRole> roles = new()
+        {
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Administrador",
+                NormalizedName = "ADMINISTRADOR"
+            },
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Usuário",
+                NormalizedName = "USUÁRIO"
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
+
+        List<IdentityUser> users = new()
+        {
+            new IdentityUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = "admin@leoflix.com",
+                NormalizedEmail = "ADMIN@LEOFLIX.COM",
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                LockoutEnabled = false,
+                EmailConfirmed = true
+            },
+            new IdentityUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Email = "user@gmail.com",
+                NormalizedEmail = "USER@GMAIL.COM",
+                UserName = "User",
+                NormalizedUserName = "USER",
+                LockoutEnabled = true,
+                EmailConfirmed = true
+            }
+        };
+        foreach (var user in users)
+        {
+            PasswordHasher<IdentityUser> pass = new();
+            user.PasswordHash = pass.HashPassword(user, "@Etec123");
+        }
+        builder.Entity<IdentityUser>().HasData(users);
+
+        List<AppUser> appUsers = new()
+        {
+            new AppUser()
+            {
+                AppUserId = users[0].Id,
+                Name = "Uellis Taku Ku Navara",
+                Birthday = DateTime.Parse("09/04/2004"),
+                Photo = ""
+            },
+            new AppUser()
+            {
+                AppUserId = users[1].Id,
+                Name = "Kauan Sant'ana",
+                Birthday = DateTime.Parse("10/11/2005"),
+            }
+        };
+        builder.Entity<AppUser>().HasData(appUsers);
+        List<IdentityUserRole<string>> userRoles = new()
+        {
+            new IdentityUserRole<string>()
+            {
+                UserId = users[0].Id,
+                RoleId = roles[0].Id
+            },
+            new IdentityUserRole<string>()
+            {
+                UserId = users[0].Id,
+                RoleId = roles[1].Id
+            },
+            new IdentityUserRole<string>()
+            {
+                UserId = users[1].Id,
+                RoleId = roles[1].Id
+            },
+        };
+        builder.Entity<IdentityUserRole<string>>().HasData(userRoles);
         #endregion
     }
 
---
+++ LeoFlix/Models/AppUser.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace LeoFlix.Models;
+
+[Table("AppUser")]
+public class AppUser
+{
+    [Key]
+    public string AppUserId { get; set; }
+    public IdentityUser UserAccount { get; set; }
+
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "Por favor, informe o Nome")]
+    [StringLength(60, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Name { get; set; }
+    [DataType(DataType.Date)]
+    [Display(Name = "Data de Nascimento")]
+    [Required(ErrorMessage = "")]
+    public DateTime Birthday {get; set;}
+
+    [StringLength(300)]
+    [Display(Name = "Foto")]
+    public string Photo { get; set; }
+    
+    public ICollection<MovieGenre> Movies { get; set; }
+}
