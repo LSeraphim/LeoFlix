f2227b8cec7f109b3d452bfb03e0659966189ba9
LSeraphim <leostart133@gmail.com>
2024-05-21 20:39:19 -0300

    Adicionado Login

--- LeoFlix/Controllers/AccountController.cs
+++ LeoFlix/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Net.Mail;
+using System.Security.Claims;
 using LeoFlix.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -38,9 +39,51 @@ public class AccountController : Controller
     {
         if (ModelState.IsValid)
         {
-            
+            string userName = login.Email;
+            if(isValidEmail(userName))
+            {
+                var user = await _userManager.FindByEmailAsync(userName);
+                if(user != null)
+                {
+                    userName = user.UserName;
+                }
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta Bloqueada! Aguarde alguns minutos para tentar novamente");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha inválidos");
+            }
         }
         return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} saiu do sistema");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult AcessDenied()
+    {
+        return View();
     }
 
     private static bool isValidEmail(string email)
